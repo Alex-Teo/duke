2675b8751899f08ddca0800a1d62aa10ff7b3e18
Alex-Teo <alex-teo@comp.nus.edu.sg>
2019-08-21 02:36:26 +0800

    Level 1 commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +7,15 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        Scanner input = new Scanner(System.in);
+        String inout = input.nextLine();
+        while (!inout.equals("bye")) {
+            System.out.println(inout);
+            inout = input.nextLine();
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
