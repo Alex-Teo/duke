cf44248755ee7020d6b39251e1c8895029ecf2be
Alex-Teo <alex-teo@comp.nus.edu.sg>
2019-08-21 11:11:05 +0800

    Level 2 commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -9,10 +12,24 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+        ArrayList<String> todo = new ArrayList<>();
         Scanner input = new Scanner(System.in);
         String inout = input.nextLine();
         while (!inout.equals("bye")) {
-            System.out.println(inout);
+            //System.out.println(inout);
+            if (inout.equals("list")) {
+                int i = 0;
+                while (i < todo.size()) {
+                    System.out.println((i + 1) + ". ");
+                    System.out.println(todo.get(i));
+                    i++;
+                }
+            }
+            else {
+                todo.add(inout);
+                System.out.println("added: ");
+                System.out.println(inout);
+            }
             inout = input.nextLine();
         }
 
