8ebc69f17415d59ab7e6e1cdb18219798a449a33
Alex-Teo <alex-teo@comp.nus.edu.sg>
2019-08-25 22:04:44 +0800

    Level 5 commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,58 +15,88 @@ public class Duke {
         ArrayList<Task> yettodo = new ArrayList<>();
         Scanner input = new Scanner(System.in);
         String inout = input.nextLine();
+        try {
+            while (!inout.equals("bye")) {
+                Task task;
+                //System.out.println(inout);
+                String[] word = inout.split(" ", 2);
+                if (inout.equals("list")) {
+                    task = new Task(inout);
+                    System.out.println("Here are the tasks in your list:");
+                    int i = 0;
+                    for (Task t : yettodo) {
+                        System.out.println((i + 1) + ". ");
+                        System.out.println(t);
+                        i++;
+                    }
+                } else if (word[0].equals("done")) {
+                    try {
+                        int num = Integer.parseInt(word[1]);
+                        yettodo.get(num - 1).Done();
+                    }
+                    catch (ArrayIndexOutOfBoundsException e) {
+                        System.out.println ("Done what?");
+                    }
+                } else if (word[0].equals("todo")) {
+                    try {
 
-        while (!inout.equals("bye")) {
-            Task task;
-            //System.out.println(inout);
-            String[] word = inout.split(" ", 2);
-            if (inout.equals("list")) {
-                task = new Task (inout);
-                System.out.println ("Here are the tasks in your list:");
-                int i = 0;
-                for (Task t: yettodo) {
-                    System.out.println((i + 1) + ". ");
-                    System.out.println(t);
-                    i++;
+                        task = new Todo(word[1]);
+                        System.out.println("Got it. I've added this task:");
+                        yettodo.add(task);
+                        System.out.println(task);
+                        System.out.println("Now you have " + yettodo.size() + ((yettodo.size() > 1) ? " tasks" : " task") + " in the list");
+                    }
+                    catch (ArrayIndexOutOfBoundsException e){
+                        System.out.println ("☹ OOPS!!! The description of a todo cannot be empty.");
+
+                    }
+                } else if (word[0].equals("deadline")) {
+                    try {
+
+                        String[] holder = word[1].split(" /by ", 2);
+                        task = new Deadline(holder[0], holder[1]);
+                        System.out.println("Got it. I've added this task:");
+                        yettodo.add(task);
+                        System.out.println(task);
+                        System.out.println("Now you have " + yettodo.size() + ((yettodo.size() > 1) ? " tasks" : " task") + " in the list");
+                    }
+                    catch (ArrayIndexOutOfBoundsException e) {
+                        System.out.println ("Wow so free is it?");
+                    }
+                } else if (word[0].equals("event")) {
+                    try {
+
+                        String[] holder = word[1].split(" /at ", 2);
+                        task = new Event(holder[0], holder[1]);
+                        System.out.println("Got it. I've added this task:");
+                        yettodo.add(task);
+                        System.out.println(task);
+                        System.out.println("Now you have " + yettodo.size() + ((yettodo.size() > 1) ? " tasks" : " task") + " in the list");
+                    }
+                    catch(ArrayIndexOutOfBoundsException e){
+                        System.out.println("Lepak?");
+                    }
                 }
+                else {
+                    try {
+                        task = new Task(inout);
+                        yettodo.add(task);
+                        System.out.println("added: ");
+                        System.out.println(inout);
+                    }
+                    catch(ArrayIndexOutOfBoundsException e){
+                        System.out.println("Being Air seems nice");
+                    }
+                }
+                inout = input.nextLine();
             }
-            else if (word[0].equals("done")) {
-                int num = Integer.parseInt(word[1]);
-                yettodo.get(num - 1).Done();
-            }
-            else if (word[0].equals("todo")) {
-                System.out.println ("Got it. I've added this task:");
-                task = new Todo (word[1]);
-                yettodo.add(task);
-                System.out.println(task);
-                System.out.println ("Now you have " + yettodo.size() + ((yettodo.size() > 1) ? " tasks" : " task") + " in the list");
-            }
-            else if (word[0].equals("deadline")) {
-                System.out.println ("Got it. I've added this task:");
-                String[] holder = word[1].split (" /by ", 2);
-                task = new Deadline (holder[0], holder[1]);
-                yettodo.add(task);
-                System.out.println(task);
-                System.out.println ("Now you have " + yettodo.size() + ((yettodo.size() > 1) ? " tasks" : " task") + " in the list");
-            }
-            else if (word[0].equals("event")) {
-                System.out.println ("Got it. I've added this task:");
-                String[] holder = word[1].split (" /at ", 2);
-                task = new Event (holder[0], holder[1]);
-                yettodo.add(task);
-                System.out.println(task);
-                System.out.println ("Now you have " + yettodo.size() + ((yettodo.size() > 1) ? " tasks" : " task") + " in the list");
-            }
-            else {
-                task = new Task (inout);
-                yettodo.add(task);
-                System.out.println("added: ");
-                System.out.println(inout);
-            }
-            inout = input.nextLine();
+
+
+            System.out.println("Bye. Hope to see you again soon!");
+
         }
-
-
-        System.out.println("Bye. Hope to see you again soon!");
+        finally {
+            System.out.println ("That's a wrap");
+        }
     }
 }
